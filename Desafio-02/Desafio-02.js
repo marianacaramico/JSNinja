a652416bf4357c546deaead32551d9ae6629abe7
Mariana Caramico <marianacaramico@hotmail.com>
2019-02-21 13:32:06 -0300

    Update Desafio-02.js

--- Desafio-02/Desafio-02.js
+++ Desafio-02/Desafio-02.js
@@ -1,19 +1,15 @@
-
 // Crie uma função que receba dois argumentos e retorne a soma dos mesmos.
 function somar(n1,n2) { return n1+n2 }
-undefined
 
 // Declare uma variável que receba a invocação da função criada acima, passando dois números quaisquer por argumento, e somando `5`
 //ao resultado retornado da função.
 var soma = somar(1,2) + 5;
 
 // Qual o valor atualizado dessa variável?
-soma;
-8
+soma; //8
 
 // Declare uma nova variável, sem valor.
 var nova;
-undefined
 
 /*
 Crie uma função que adicione um valor à variável criada acima, e retorne a string:
@@ -21,14 +17,12 @@ Crie uma função que adicione um valor à variável criada acima, e retorne a s
 Onde VALOR é o novo valor da variável.
 */
 function adicionar(){ nova = 3; return 'O valor da variável agora é '+nova; }
-undefined
 
 // Invoque a função criada acima.
 adicionar();
-"O valor da variável agora é 3"
 
 // Qual o retorno da função? (Use comentários de bloco).
-"O valor da variável agora é 3"
+//"O valor da variável agora é 3"
 
 /*
 Crie uma função com as seguintes características:
@@ -39,22 +33,20 @@ Crie uma função com as seguintes características:
 */
 function argumentos(n1,n2,n3){
     if(n1===undefined || n2===undefined || n3===undefined) return 'Preencha todos os valores corretamente!';
-	return n1*n2*n3 + 2;
+    return n1*n2*n3 + 2;
 }
 
 // Invoque a função criada acima, passando só dois números como argumento.
 argumentos(2,4);
-"Preencha todos os valores corretamente!"
 
 // Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
-"Preencha todos os valores corretamente!"
+//"Preencha todos os valores corretamente!"
 
 // Agora invoque novamente a função criada acima, mas passando todos os três argumentos necessários.
 argumentos(1,2,3);
-8
 
 // Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
-8
+//8
 
 /*
 Crie uma função com as seguintes características:
@@ -74,14 +66,7 @@ function tres(a,b,c){
 }
 
 // Invoque a função acima utilizando todas as possibilidades (com nenhum argumento, com um, com dois e com três.)
-tres();
-false
-
-tres(7);
-7
-
-tres(7,3);
-10
-
-tres(7,3,2);
-5
+tres(); //false
+tres(7); //7
+tres(7,3); //10
+tres(7,3,2); //5
